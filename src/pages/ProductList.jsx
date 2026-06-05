9977b45a1e21bb640c6cd7de2b2d220fdbd7463c
Alessandro Ledda <aleledda75@icloud.com>
2026-06-05 09:35:56 +0200

    feat filter to category + fix in file productList.jsx

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -11,25 +11,47 @@ function ProductList() {
     // setto var di stato per gestione ordimanto + string literal ('none', 'asc' (A-Z), 'desc' (Z-A))
     const [sortOrder, setSortOrder] = useState('none');
 
+    // setto var di stato per filtraggio per categoria, inilmente vuota o 'all' per mostrare tutte le categorie, quando l'utente seleziona una categoria specifica aggiorno lo stato con il valore selezionato 
+    const [categories, setCategories] = useState('all');
+
+
+    // esstraggo dinamicamente le categorie uniche dai prodotti, utilizzo Set per ottenere solo valori unici e spread operator per trasformare il Set in un array
+    const uniqueCategory = useMemo(() => {
+        const allCategories = products.map((product) => product.category);
+        return [...new Set(allCategories)];
+    }, [products]);
+
 
     // utilizzo usememo per rendere più efficiente il filtraggio e l'ordinamento dei prodotti, in questo modo la funzione viene eseguita solo quando cambia lo stato dei prodotti, della search bar o dell'ordinamento
     // wrappo la funzione di filtraggio e ordinamento dei prodotti in useMemo
     const processedProducts = useMemo(() => {
+        let result = [...products];
+
         // funzione per filtrare i prodotti in base al valore della search bar, converto sia il titolo del prodotto che il valore della search bar in minuscolo per rendere la ricerca case-insensitive
-        let filteredProduct = products.filter((product) => {
-            return product.title.toLowerCase().includes(searchBar.toLowerCase())
-        })
+        if (searchBar) {
+            result = result.filter((product) => {
+                return product.title.toLowerCase().includes(searchBar.toLowerCase())
+            })
+        }
 
         // gestione ordinamento
         if (sortOrder === 'asc') {
             // ordino i prodotti in ordine alfabetico in base al titolo, utilizzo localeCompare per confrontare le stringhe
-            filteredProduct = [...filteredProduct].sort((a, b) => a.title.localeCompare(b.title));
+            result = [...result].sort((a, b) => a.title.localeCompare(b.title));
         } else if (sortOrder === 'desc') {
             // ordino i prodotti in ordine inverso in base al titolo
-            filteredProduct = [...filteredProduct].sort((a, b) => b.title.localeCompare(a.title));
+            result = [...result].sort((a, b) => b.title.localeCompare(a.title));
         }
-        return filteredProduct;
-    }, [products, searchBar, sortOrder]);
+
+        // gestione filtraggio per categoria, solo se l'utente ha selezionato una categoria specifica (diversa da 'all') filtro i prodotti in base alla categoria, converto sia la categoria del prodotto che il valore selezionato in minuscolo per rendere il filtraggio case-insensitive
+        if (categories && categories !== 'all') {
+            result = result.filter((product) => {
+                return product.category.toLowerCase() === categories.toLowerCase();
+            });
+        }
+
+        return result;
+    }, [products, searchBar, sortOrder, categories]);
 
 
 
@@ -87,6 +109,16 @@ function ProductList() {
                 onClick={() => setSortOrder('none')}
             >Resetta ordinamento
             </button>
+
+            {/* creo la select per filtraggio per categoria */}
+            <select value={categories} onChange={(e) => setCategories(e.target.value)}>
+                <option value="all">Tutte le categorie</option>
+                {uniqueCategory.map((category) => (
+                    <option key={category} value={category}>
+                        {category}
+                    </option>
+                ))}
+            </select>
 
             <div>
                 {/* controllo prodotti  */}
